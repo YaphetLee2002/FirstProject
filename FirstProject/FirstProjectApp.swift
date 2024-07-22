ea1c2db85c18bdd5cbd6a93b3bfb14aeee1d1924
Yaphet <YaphetLee2002@qq.com>
2024-07-22 13:38:04 +0800

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by 李亚赟 on 2024/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
